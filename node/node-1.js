53e018475a1514949439255eed03077bc81c914c
liwei <2212261902@qq.com>
2022-07-25 19:17:16 +0800

    node fs读取和写入数据

---
+++ node/node-1.js
@@ -0,0 +1,19 @@
+//导入fs模块
+let fs = require("fs")
+
+//读取
+fs.readFile("./file/1.text", "utf-8", function (err, dataStr) {
+    if (err) {
+        return console.log("读取失败" + err.message);
+    }
+    console.log("读取成功" + dataStr);
+    let oldArr = dataStr.split(" ")
+    let arrNew = []
+    oldArr.forEach(item => {
+        arrNew.push(item.replace("=", "："))
+    })
+    //转换成字符串
+    arrNew = arrNew.join("\r\n")
+    //写入
+    fs.writeFile("./file/2.text", arrNew, "utf-8", function (err) { })
+})
